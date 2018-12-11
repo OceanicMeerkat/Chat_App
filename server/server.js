6dd14de58c04cfd3a7539407ac09ce508c1567d5
OceanicMeerkat <pai.suparanon@gmail.com>
2018-12-11 22:37:27 +0700

    Added socket io

--- server/server.js
+++ server/server.js
@@ -1,24 +1,26 @@
 const path = require('path');
-
+const http = require('http');
 const publicPath = path.join(__dirname,'../public')
+const socketIO = require('socket.io');
 
-/*
-console.log(__dirname + '/../public');
-console.log(publicPath);
-*/
+
 const express = require('express');
 const app = express();
-const port = process.env.PORT || port;
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
 
 app.use(express.static(publicPath));
 
-/*
-app.get('*', (req, res) => {
-    res.sendFile(`index.html`, { root: www });
-});
-*/
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
+    socket.on('disconnect', () => {
+        console.log('The user was disconnected');
+    })
 
-app.listen(port, () => console.log(`listening on ${port}`));
+})
+
+server.listen(port, () => console.log(`listening on ${port}`));
