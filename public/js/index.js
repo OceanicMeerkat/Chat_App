d4de110ab39f4a7cf5b717fc00c488aa23362c84
OceanicMeerkat <pai.suparanon@gmail.com>
2018-12-19 11:14:43 +0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Andrew',
-        text:'This is test Texts'
-    })
 })
 socket.on('disconnect', function ()  {
     console.log('Disconnected from server');
