638240b3210c74317cca5a9a15c542f9973d5454
OceanicMeerkat <pai.suparanon@gmail.com>
2019-01-02 13:30:36 +0700

    Add moment

---
+++ playground/time.js
@@ -0,0 +1,16 @@
+var moment = require('moment');
+
+
+var someTimeStamp = moment().valueOf()
+console.log(someTimeStamp);
+
+var createdAt = 1234;
+var date = moment(createdAt);
+
+/*
+date.add(100, 'years').subtract(9, 'months');
+console.log(date.format("MMM Do, YYYY"))
+
+*/
+
+console.log(date.format('h:mm a'));
